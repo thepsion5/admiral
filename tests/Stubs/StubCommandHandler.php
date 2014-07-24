969493271c4ddca7506f5db896284ab1c1c941c1
Sean Mumford <sean.mumford@gmail.com>
2014-07-24 13:32:52 +0000

    derp

--- tests/Stubs/StubCommandHandler.php
+++ tests/Stubs/StubCommandHandler.php
@@ -1,13 +1,14 @@
 <?php
 namespace Thepsion5\Admiral\Testing\Stubs;
 
-use Thepsion5\Admiral\CommandHandlerInterface;
+use Thepsion5\Admiral\CommandHandlerInterface,
+    Thepsion5\Admiral\CommandInterface;
 
 class StubCommandHandler implements CommandHandlerInterface
 {
     public $handled = array();
 
-    public function handle($command)
+    public function handle(CommandInterface $command)
     {
         $handled[] = get_class($command);
         return $this;
